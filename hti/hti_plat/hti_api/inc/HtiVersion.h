40403862f8a4254ae49af8d1009243465700b25d
hgs <none@none>
2010-05-25 18:01:01 +0300

    201021

--- hti/hti_plat/hti_api/inc/HtiVersion.h
+++ hti/hti_plat/hti_api/inc/HtiVersion.h
@@ -25,13 +25,13 @@
 // CONSTANTS
 
 const TUint8 KHtiVersionMajor = 2;
-const TUint8 KHtiVersionMinor = 22;
+const TUint8 KHtiVersionMinor = 23;
 const TUint8 KHtiVersionBuild = 0;
 
 const TUint16 KHtiVersionYear  = 2010;
-const TUint8  KHtiVersionMonth = 4;
-const TUint8  KHtiVersionWeek  = 17;
-const TUint8  KHtiVersionDay   = 30;
+const TUint8  KHtiVersionMonth = 5;
+const TUint8  KHtiVersionWeek  = 19;
+const TUint8  KHtiVersionDay   = 14;
 
 //  MACROS
 
